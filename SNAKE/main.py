a916368f10556707e227cf6c1e4d3023b93be546
Brendon Esteves <51219353+Unckros@users.noreply.github.com>
2023-09-22 19:59:29 -0400

    Inicialização cód main.py

---
+++ SNAKE/main.py
@@ -0,0 +1,12 @@
+#!/bin/python3 env
+
+import pygame
+
+WINDOW_SIZE = (600, 600)
+
+pygame.init()
+screen = pygame.display.set_mode((WINDOW_SIZE))
+pygame.display.set_caption('Snake')
+
+while True:
+    pass
