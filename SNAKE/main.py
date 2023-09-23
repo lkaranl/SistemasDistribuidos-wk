82ea143f5b78de108a982f11cb4ccaecda7f6b42
Brendon Esteves <51219353+Unckros@users.noreply.github.com>
2023-09-22 22:36:53 -0400

    adição de explicação superficial de como o socket deve funcionar de inicio

--- SNAKE/main.py
+++ SNAKE/main.py
@@ -3,10 +3,28 @@
 import pygame
 
 WINDOW_SIZE = (600, 600)
+PIXEL_SIZE = 10
 
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_SIZE))
 pygame.display.set_caption('Snake')
 
+snake_pos = [(250, 50), (260, 50), (270, 50)]
+snake_surface = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
+snake_surface.fill((255, 255, 255))
+
+apple_surface = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
+apple_surface.fill((255, 0, 0))
+
 while True:
-    pass
+    screen.fill(())
+    for event in pygame.event.get():
+        if event.type == quit:
+            pygame.quit()
+            quit()
+    for pos in snake_pos:
+        screen.blit(snake_surface, pos)
+
+    snake_pos[0] = snake_pos[0] [0] + 10, snake_pos[0] [1]
+
+    pygame.display.update()
---
+++ Socket/README.md
@@ -0,0 +1,2 @@
+# Funcionamento do socket
+Ele tem que enviar pacotes x para todos os nós do grupo e implementar isso \n emtodas as estações do grupo, ele vai definir o grupo pelo endereço de ip, e \n e passar essa informação ao main.py que vai fazer o tratamento da informação.
